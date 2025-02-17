6bb3435a42a9e3624afe903044c96e4e8932eec0
廖禹璇 <100425158+hsuan619@users.noreply.github.com>
2025-02-17 21:10:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ from flask import Flask, render_template, request, redirect, url_for
 import pandas as pd
 
 app = Flask(__name__)
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
 # 讀取CSV檔案
 def read_csv():
     return pd.read_csv("wordSearch.csv")
@@ -55,4 +53,4 @@ def add_submit():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000)
